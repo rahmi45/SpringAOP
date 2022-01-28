e2a0a18ca23f251f1e2914f29c2f29afcb362d6d
RAHMI <rachid.rahmi@atos.net>
2022-01-28 10:19:01 +0100

    @PointCut #1

--- src/com/spring/aop/DataBase.java
+++ src/com/spring/aop/DataBase.java
@@ -2,16 +2,24 @@ package com.spring.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DataBase {
 	
-	@Before("execution(public * com.spring.dao.*.*(..))")
+	@Pointcut("execution(public * com.spring.dao.*.*(..))")
+	public void myPointCut() {}
+	
+	@Before("myPointCut()")
 	public void connectionDB() {
 		System.out.println("Connected !...");
 	}
 	
+	@Before("myPointCut()")
+	public void logIn() {
+		System.out.println("Log In.............true!");
+	}
 
 }
